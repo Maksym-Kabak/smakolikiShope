187d63d9344d9722b8793bab4721061a70e18a0d
Maksym-Kabak <maksitheslayer@gmail.com>
2020-09-29 22:41:44 +0300

    Added login an logout features

--- dist/routes/product/product.router.js
+++ dist/routes/product/product.router.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.productRouter = void 0;
 const express_1 = require("express");
 const controller_1 = require("../../controller");
-const user_1 = require("../../middleware/user");
+const middleware_1 = require("../../middleware");
 const router = express_1.Router();
-router.post('/', user_1.checkIsEmailExistsMiddleware, controller_1.productController.createProduct);
+router.post('/', middleware_1.checkAccessTokenMiddleware, controller_1.productController.createProduct);
 exports.productRouter = router;
 //# sourceMappingURL=product.router.js.map
--- src/routes/product/product.router.ts
+++ src/routes/product/product.router.ts
@@ -1,9 +1,9 @@
 import {Router} from 'express';
 import {productController} from '../../controller';
-import {checkIsEmailExistsMiddleware} from '../../middleware/user';
+import {checkAccessTokenMiddleware} from '../../middleware';
 
 const router = Router();
 
-router.post('/', checkIsEmailExistsMiddleware, productController.createProduct);
+router.post('/', checkAccessTokenMiddleware, productController.createProduct);
 
 export const productRouter = router;
